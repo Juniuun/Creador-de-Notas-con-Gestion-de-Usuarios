baa61e790ba7ca8f74f674949887f34e8bd7c78d
Juniuun <sarafox_2006@hotmail.com>
2025-03-28 21:11:41 +0000

    Añadiendo la clase Main de dónde se ejecutará el programa

---
+++ CreadorDeNotas/src/Main.java
@@ -0,0 +1,9 @@
+import jbl.LoginManager;
+
+// Clase principal que inicia la aplicación
+public class Main {
+    public static void main(String[] args) {
+        // Crea y muestra la ventana de inicio de sesión
+        new LoginManager();
+    }
+}
